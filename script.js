1fb61ae2aabef6377a5ce175aa5a42726ce31d88
Sid Gangrade <39662330+sidgang324@users.noreply.github.com>
2023-10-30 10:00:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 const content = document.getElementById('content');
 const refreshButton = document.getElementById('refreshButton');
 
-// Array of image filenames in your "images" folder
-const images = [
-    'images/meme1.jpg',
-    'images/meme2.jpg',
-    'images/spooky1.jpg',
-    'images/spooky2.jpg'
-    // Add more image filenames here
-];
+// Create an array of image filenames in your "images" folder using a for loop
+const images = [];
+const numImages = 9; // Change this to the total number of images
+
+for (let i = 1; i <= numImages; i++) {
+    images.push(`images/${i}.jpg`);
+}
 
 function getRandomItem(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function displayRandomContent() {
-    const random = Math.random();
+function displayRandomImage() {
     const randomImage = getRandomItem(images);
     const imageUrl = window.location.href + randomImage;
 
-    if (random < 0.5) {
-        content.innerHTML = `<img src="${imageUrl}" alt="Meme">`;
-    } else {
-        content.innerHTML = `<img src="${imageUrl}" alt="Spooky Pic">`;
-    }
+    content.innerHTML = `<img src="${imageUrl}" alt="Image">`;
 }
 
-refreshButton.addEventListener('click', displayRandomContent);
-displayRandomContent();
+refreshButton.addEventListener('click', displayRandomImage);
+displayRandomImage();
