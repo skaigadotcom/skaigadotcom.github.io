4605ebcb481e733b85b98e96cde46dea77a7f464
Sid Gangrade <39662330+sidgang324@users.noreply.github.com>
2023-10-30 11:58:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const content = document.getElementById('content');
 const refreshButton = document.getElementById('refreshButton');
 
 const images = [];
-const numImages = 13;
+const numImages = 20;
 
 for (let i = 1; i <= numImages; i++) {
     images.push(`images/${i}.jpg`);
