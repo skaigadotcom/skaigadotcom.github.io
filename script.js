ce8e517e814b2272fc6000ebed0ae727d4b5d8ac
Sid Gangrade <39662330+sidgang@users.noreply.github.com>
2023-10-30 15:05:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -22,41 +22,23 @@ function displayRandomImage() {
 refreshButton.addEventListener('click', displayRandomImage);
 displayRandomImage();
 
-document.addEventListener('touchstart', (event) => {
-    const candyEmoji = document.createElement('div');
-    candyEmoji.textContent = '🍬';
-    candyEmoji.classList.add('candy-emoji');
-    document.body.appendChild(candyEmoji);
+document.addEventListener('DOMContentLoaded', () => {
+    const candy = document.querySelector('.falling-candy');
 
-    const x = event.touches[0].clientX;
-    const y = event.touches[0].clientY;
-    candyEmoji.style.left = x + 'px';
-    candyEmoji.style.top = y + 'px';
+    // Set initial random horizontal velocity
+    const randomHorizontalVelocity = Math.random() > 0.5 ? '2px' : '-2px';
+    candy.style.setProperty('--horizontal-velocity', randomHorizontalVelocity);
 
-    const startTime = performance.now();
-    const initialY = y;
-    const initialX = x;
-    const initialVerticalVelocity = -10; // Initial positive velocity
-    const duration = 2000;
-    const gravity = 9.8;
-    const horizontalVelocity = (Math.random() < 0.5 ? 1 : -1) * 2;
+    // Set initial random vertical velocity
+    const initialVerticalVelocity = Math.random() * 4 - 8;
+    candy.style.transform = `translateY(-10vh) translateX(${randomHorizontalVelocity})`;
 
-    function updatePosition() {
-        const currentTime = performance.now();
-        const elapsed = currentTime - startTime;
-        const verticalVelocity = initialVerticalVelocity + gravity * (elapsed / 1000);
-        const newY = initialY + verticalVelocity * (elapsed / 1000) + 0.5 * gravity * (elapsed / 1000) ** 2;
-        const newX = initialX + horizontalVelocity * (elapsed / 1000);
-        candyEmoji.style.top = newY + 'px';
-        candyEmoji.style.left = newX + 'px';
+    // Apply the falling animation
+    candy.style.animation = `candy-fall ${Math.random() * 3 + 2}s ease-in infinite`;
 
-        if (elapsed < duration) {
-            requestAnimationFrame(updatePosition);
-        } else {
-            candyEmoji.remove();
-        }
-    }
-
-    requestAnimationFrame(updatePosition);
+    // Add click event to reposition the candy
+    document.addEventListener('click', (e) => {
+        const touchX = e.clientX;
+        candy.style.transform = `translate(${touchX}px, -10vh)`;
+    });
 });
-
