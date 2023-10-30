859021ee202979670bbfd1e9421561a2cea348ab
Sid Gangrade <39662330+sidgang@users.noreply.github.com>
2023-10-30 14:45:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,23 @@ document.addEventListener('touchstart', (event) => {
     candyEmoji.style.left = x + 'px';
     candyEmoji.style.top = y + 'px';
 
-    setTimeout(() => {
-        candyEmoji.style.transform = 'scale(2) translateY(100vh)';
-        candyEmoji.style.opacity = '0';
-    }, 100); // Adjust the duration as needed
+    const startTime = performance.now(); // Get the start time in milliseconds
+    const initialY = y;
+    const duration = 2000; // Adjust the duration as needed
+    const gravity = 9.8; // Acceleration due to gravity in m/s^2
+
+    function updatePosition() {
+        const currentTime = performance.now();
+        const elapsed = currentTime - startTime;
+        const newY = initialY + 0.5 * gravity * (elapsed / 1000) ** 2;
+        candyEmoji.style.top = newY + 'px';
+
+        if (elapsed < duration) {
+            requestAnimationFrame(updatePosition);
+        } else {
+            candyEmoji.remove(); // Remove the emoji when the animation is complete
+        }
+    }
+
+    requestAnimationFrame(updatePosition);
 });
